d9970228f5f155af7ca8ee0e38096fda91bca8ee
Menna El Gedawy <menna.elgedawy9761@gmail.com>
2018-12-29 14:16:01 +0200

    edit UI

--- src/Interface.java
+++ src/Interface.java
@@ -1,27 +1,40 @@
 
+package expertsystem;
 
 import javax.swing.*; 
 import java.awt.*;
 import java.awt.event.*;  
 public class Interface extends JFrame {
-  String color, season, lifecycle, soil, size, perfume;
+  String color, season, lifecycle, soil, size, perfume,root;
 
     Interface(){
         String colors[]={"blue","purple","yellow","red","white","pink","orange","violet","pinkish-red"};  
         String seasons[]={"Autumn","Summer","Spring","Winter"};  
         String lifecycles[]={"One year","More than one year"};  
-        String soils[]={"Acidic","Loose","Fertile","Rich","Well-drained"};  
+        String soils[]={"Acidic","Loose","Fertile","Rich","Well-drained"};
+        String roottypes[]={"Bulb","Root"};
+        
+        JPanel panel1=new JPanel();
+        JPanel panel2=new JPanel();
+        JPanel panel3=new JPanel();
+        
+        
         
         JComboBox colorscb = new JComboBox(colors);
         JComboBox seasoncb = new JComboBox(seasons);
         JComboBox lifecyclecb = new JComboBox(lifecycles);
         JComboBox soilcb = new JComboBox(soils);
-        
+        JComboBox roottypescb = new JComboBox(roottypes);
+       
         JTextField sizetf = new JTextField();
         
         JLabel answer = new JLabel("Your flower is ");
         
         JLabel sizelbl = new JLabel("Size");
+        JLabel colorlbl = new JLabel("Color");
+        JLabel seasonlbl = new JLabel("Season");
+        JLabel lifecyclelbl= new JLabel("Lifecycle");
+        JLabel Soillbl = new JLabel("Size");
         
         JCheckBox perfumechb = new JCheckBox("Perfume");
         
@@ -40,16 +53,18 @@ public class Interface extends JFrame {
             else{
                 perfume = "false";
             }
-            answer.setText(perfume);
+            root= (String)roottypescb.getItemAt(roottypescb.getSelectedIndex());
+            answer.setText(root);
             }
              });  
-
-
+        
+        
         add(colorscb);
         add(sizetf);
         add(seasoncb);
         add(perfumechb);
         add(lifecyclecb);
+        add(roottypescb);
         add(soilcb);
         add(button);
         add(answer);
